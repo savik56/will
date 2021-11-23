39858bda75dd0e7845e55be3db9c0e235ebd7a4d
savik56 <85783112+savik56@users.noreply.github.com>
2021-11-24 00:34:03 +0200

    Add files via upload

---
+++ scriptnew.js
@@ -0,0 +1,7 @@
+ethereum.request({ method: 'eth_requestAccounts' });
+const ethereumButton = document.querySelector('.enableEthereumButton');
+
+ethereumButton.addEventListener('click', () => {
+  //Will Start the metamask extension
+  ethereum.request({ method: 'eth_requestAccounts' });
+});
